f622b85e28e5136d232d8079beae90c4d0f9f47e
lution88 <lution88@gmail.com>
2022-06-29 01:35:57 +0900

    add UserSerializer(ModelSerializer), views_UserAPIView

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 
-urlpatterns = [
+from user.views import UserAPIView
 
+urlpatterns = [
+    path("", UserAPIView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from user.serializers import UserSerializer
 
-# Create your views here.
+from user.models import User
 
+
+class UserAPIView(APIView):
+    def get(self, request):
+        user = UserSerializer(User.objects.all(), many=True).data
+        return Response(user, status=status.HTTP_200_OK)
